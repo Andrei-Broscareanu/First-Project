1ce9ac1cf66efb41ddbce20128e13f1c47154ae9
Andrei <andreibroscareanu@yahoo.com>
2023-01-20 15:26:22 +0200

    Initial Commit

---
+++ app/Models/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Product extends Model
+{
+    use HasFactory, HasApiTokens, Notifiable;
+
+
+    protected $fillable = [
+        'name',
+        'price',
+        'password',
+    ];
+
+
+    protected $casts = [
+        'product_verified_at' => 'datetime',
+    ];
+}
